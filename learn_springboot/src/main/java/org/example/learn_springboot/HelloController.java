116ffbbb1df23aed4600f65b72c27c4f2e7298fe
sue <shoh@maxst.com>
2024-08-21 18:35:55 +0900

    spring DI

--- learn_springboot/src/main/java/org/example/learn_springboot/HelloController.java
+++ learn_springboot/src/main/java/org/example/learn_springboot/HelloController.java
@@ -3,10 +3,19 @@ package org.example.learn_springboot;
 import java.util.Objects;
 
 public class HelloController {
-	public String hello(String name) {
-		SimpleHelloService simpleHelloService = new SimpleHelloService();
 
-		return simpleHelloService.sayHello(Objects.requireNonNull(name));
+	private final HelloService helloService;
+	//final 이니까 생성자나 어디서나 초기화가 필요하다고 컴파일 에러가 발생한다.
+
+	public HelloController(HelloService helloService) {
+		this.helloService = helloService;
+	}
+
+	public String hello(String name) {
+		//SimpleHelloService simpleHelloService = new SimpleHelloService();
+		//직접 인스턴스를 만들지 않고 어셈블러에 의해 헬로 컨트롤러 클래스의 객체를 만들때 생성자 파라미터로 주입할 수 있도록 변경 하자
+
+		return helloService.sayHello(Objects.requireNonNull(name));
 		// 만약 null 이면 예외를 던진다.
 		// null 인 경우를 방지 하고 null 이 아닐 때만 사용 가능
 	}
--- learn_springboot/src/main/java/org/example/learn_springboot/LearnSpringbootApplication.java
+++ learn_springboot/src/main/java/org/example/learn_springboot/LearnSpringbootApplication.java
@@ -40,7 +40,7 @@ public class LearnSpringbootApplication {
 
 		/**
 		 * 2. 요청을 front controller 를 통해서 받아서 처리하기
-		 */
+
 		webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
@@ -68,10 +68,10 @@ public class LearnSpringbootApplication {
 				}
 			}).addMapping("/*"); // 모든 요청에 대해 처리를 하겠다는 와일드 카드
 		});
-
+		 */
 		/**
 		 * 3 spring container 사용하기
-		 */
+
 		// spring container를 만들고
 		GenericApplicationContext genericApplicationContext= new GenericApplicationContext();
 		// 어떤 클래스를 이용해 bean 메타 정보를 만들 것인가가 필요함
@@ -92,6 +92,35 @@ public class LearnSpringbootApplication {
 				}
 			}
 		}).addMapping("/hello");
+		});
+
+		 */
+		/**
+		 * 4. helloService 타입의 클래스를 빈으로 등록해서 DI 로 자동으로 주입하게 하자
+		 */
+
+		GenericApplicationContext genericApplicationContext= new GenericApplicationContext();
+		genericApplicationContext.registerBean(HelloController.class);
+		// genericApplicationContext.registerBean(HelloService.class); // 인터페이스지 클래스가 아님
+		genericApplicationContext.registerBean(SimpleHelloService.class);
+		//초창기에는 xml으로 빈을 지정해서 생성자에 어떤 빈을 주입할지 정보도 다 기술을 해줬어야했다
+
+		genericApplicationContext.refresh();
+
+		webServer= serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
+						String name = req.getParameter("name");
+
+						HelloController helloController = genericApplicationContext.getBean(HelloController.class);
+						String result = helloController.hello(name);
+
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(result);
+					}
+				}
+			}).addMapping("/hello");
 		});
 
 		webServer.start();
