aa65486348b3878eee142bf099163a53d9a430fa
sue <shoh@maxst.com>
2024-08-22 11:20:59 +0900

    se application context

--- learn_springboot/src/main/java/org/example/learn_springboot/HelloController.java
+++ learn_springboot/src/main/java/org/example/learn_springboot/HelloController.java
@@ -2,18 +2,16 @@ package org.example.learn_springboot;
 
 import java.util.Objects;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@MyComponent
-// @RestController // 5. dispatcher 서블릿과는 관련이 없다., 이게 붙으면 아래의 모든 메서드가 @ResponseBody 가 붙여져 있다고 하는 것과 같다.
+@RestController // 5. dispatcher 서블릿과는 관련이 없다., 이게 붙으면 아래의 모든 메서드가 @ResponseBody 가 붙여져 있다고 하는 것과 같다.
 //@RequestMapping // 5. 디스패쳐 서블릿이 모든 메소드를 다 뒤지면 느리니까 클래스에 컨트롤러로 명시해 줘서 빠르게 찾도록
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
 
 	private final HelloService helloService;
 	//final 이니까 생성자나 어디서나 초기화가 필요하다고 컴파일 에러가 발생한다.
@@ -27,7 +25,7 @@ public class HelloController {
 	// 만들어둔 맵핑 테이블을 통해 요청을 처리한다.
 	// 그런데
 	// @RequestMapping(value = "/hello", method = RequestMethod.GET) : 옛날에 쓰던 방식
-	@ResponseBody
+	// @ResponseBody
 	public String hello(String name) {
 		//SimpleHelloService simpleHelloService = new SimpleHelloService();
 		//직접 인스턴스를 만들지 않고 어셈블러에 의해 헬로 컨트롤러 클래스의 객체를 만들때 생성자 파라미터로 주입할 수 있도록 변경 하자
@@ -39,5 +37,10 @@ public class HelloController {
 		// 5. 디스패쳐 서블릿은 리턴된 String을 보고 view를 리턴하라고 하기 때문에 view 를 찾는다.
 		//    원하는 작업에 맞는 view 가 없으니 404에러, 결과 String 을 그대로 바디에 넣어서 응답하기 위해서는
 		//    @ResponseBody
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		System.out.println(applicationContext);
 	}
 }
--- learn_springboot/src/main/java/org/example/learn_springboot/LearnSpringbootApplication.java
+++ learn_springboot/src/main/java/org/example/learn_springboot/LearnSpringbootApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan // 8. 컴포넌트가 붙은 클래스를 찾아서 등록해달라
@@ -17,7 +16,6 @@ public class LearnSpringbootApplication {
 	public static void main(String[] args) {
 
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = null;
 		/**
 		 * 1. 서블릿 띄우기
 
@@ -129,21 +127,21 @@ public class LearnSpringbootApplication {
 		 * 맵핑과 특정 컨트롤러로 바인딩해 보내주는 역할을 서블릿 코드 안에서 하고 있다.
 		 * 디스패쳐 서블릿을 통해 개발해보자
 
-		GenericWebApplicationContext genericWebApplicationContext = new GenericWebApplicationContext();
-		genericWebApplicationContext.registerBean(HelloController.class);
-		genericWebApplicationContext.registerBean(SimpleHelloService.class);
-		//dispatcher servlet 은 GenericWebApplicationContext 를 사용해야한다.
+		 GenericWebApplicationContext genericWebApplicationContext = new GenericWebApplicationContext();
+		 genericWebApplicationContext.registerBean(HelloController.class);
+		 genericWebApplicationContext.registerBean(SimpleHelloService.class);
+		 //dispatcher servlet 은 GenericWebApplicationContext 를 사용해야한다.
 
-		genericWebApplicationContext.refresh();
+		 genericWebApplicationContext.refresh();
 
-		webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-				new DispatcherServlet(genericWebApplicationContext)
-				//디스패쳐 서블릿에게 어떤 요청이 어떤 빈을 처리하라고 알려줘야함
-				// 1. xml , 2. mapping 정보를 처리할 컨트롤러 클래스 안에 코드로 맵핑 정보를 넣는 것.
-				// 2번은 컨트롤러에 맵핑 정보를 추가해준다. @GetMapping
-			).addMapping("/*");
-		});
+		 webServer = serverFactory.getWebServer(servletContext -> {
+		 servletContext.addServlet("dispatcherServlet",
+		 new DispatcherServlet(genericWebApplicationContext)
+		 //디스패쳐 서블릿에게 어떤 요청이 어떤 빈을 처리하라고 알려줘야함
+		 // 1. xml , 2. mapping 정보를 처리할 컨트롤러 클래스 안에 코드로 맵핑 정보를 넣는 것.
+		 // 2번은 컨트롤러에 맵핑 정보를 추가해준다. @GetMapping
+		 ).addMapping("/*");
+		 });
 		 */
 		/**
 		 * 6. 스프링 컨테이너로 통합
@@ -176,7 +174,35 @@ public class LearnSpringbootApplication {
 		/** 7. bean 을 등록해서 스프링 컨테이너 내의 bean 이 다른 컴포넌트를 사용한다면 어느시점에 의존시켜줄 것인가를 설정해줄 수 있다.
 		 * 팩토리 메소드를 사용해서 빈 오브젝트를 다 생성하고 의존관계 주입도 다 하고 리턴하는 오브젝트를 스프링 컨테이너에게 빈으로 등록해서 나중에 사용해 하면 되는 것
 		 * 어떤 경우에는 빈 오브젝트를 만들고 초기화 하는 작업이 복잡할때가 있다. 이 복잡한 설정을 자바 코드로 만들면 간결해진다.
+
+		 AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext() {
+		 // 어노테이션이 붙은 것들을 이용해 구성 정보를 가져오는 클래스
+
+		 @Override protected void onRefresh() {
+		 super.onRefresh();
+
+		 ServletWebServerFactory servletFactory = new TomcatServletWebServerFactory();
+
+		 WebServer dispatcherServlet = servletFactory.getWebServer(servletContext -> {
+		 servletContext.addServlet("dispatcherServlet",
+		 new DispatcherServlet(this) //this = genericWebApplicationContext
+		 ).addMapping("/*");
+		 });
+		 dispatcherServlet.start();
+		 }
+		 };
+		 // genericWebApplicationContext.registerBean(HelloController.class); //필요 없어짐, 빈을 어노테이션으로 등록하므로
+		 // genericWebApplicationContext.registerBean(SimpleHelloService.class);
+		 annotationConfigWebApplicationContext.register(LearnSpringbootApplication.class);
+		 annotationConfigWebApplicationContext.refresh();
 		 */
+
+		/**
+		 * 8. Bean 의 생명주기 메소드
+		 * 톰캣 서블릿 팩토리와 디스패쳐 서블릿 두개가 있는데 이게 없으면 시작할수가 없다.
+		 * 이 두개의 오브젝트도 스프링에 빈으로 등록해서 컨테이너가 이를 관리하게 하자
+		 */
+
 		AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = new AnnotationConfigWebApplicationContext() {
 			// 어노테이션이 붙은 것들을 이용해 구성 정보를 가져오는 클래스
 
@@ -184,14 +210,18 @@ public class LearnSpringbootApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 
-				ServletWebServerFactory servletFactory = new TomcatServletWebServerFactory();
-
-				WebServer dispatcherServlet = servletFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(this) //this = genericWebApplicationContext
-					).addMapping("/*");
+				ServletWebServerFactory servletFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// dispatcherServlet.setApplicationContext(this);
+				// 애플리케이션 컨텍스트를 지정해주지 않아도 잘 동작한다. 디스패쳐 서블릿이 어떻게 컨트롤러를 찾아 맵핑 정보를 찾는지?
+				// 스프링 컨테이너가 디스패쳐 서블릿은 애플리케이션 컨텍스트가 필요하구나 하고 알아서 주입을 해준다.
+				// ApplicationContextAware 인터페이스를 구현한 클래스가 스프링에 빈으로 등록이 되면
+				// 스프링 컨테이너는 인터페이스의 setter 를 통해서 주입해 줌
+				WebServer webServer = servletFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+						.addMapping("/*");
 				});
-				dispatcherServlet.start();
+				webServer.start();
 			}
 		};
 		// genericWebApplicationContext.registerBean(HelloController.class); //필요 없어짐, 빈을 어노테이션으로 등록하므로
@@ -201,15 +231,25 @@ public class LearnSpringbootApplication {
 	}
 
 	/**
-	// 팩토리 메소드
-	@Bean //7.스프링에게 빈이라고 알려주기
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
+	 * // 팩토리 메소드
+	 *
+	 * @Bean //7.스프링에게 빈이라고 알려주기
+	 * public HelloController helloController(HelloService helloService) {
+	 * return new HelloController(helloService);
+	 * }
+	 * @Bean public HelloService helloService() {
+	 * return new SimpleHelloService(); // 빈을 주입받을때 어떤 타입을 기대하는가?
+	 * }
+	 */
+
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
 	}
 
 	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService(); // 빈을 주입받을때 어떤 타입을 기대하는가?
+	public DispatcherServlet dispatcherServlet() {
+		// 자기가 이용할 컨트롤러를 찾아야해서 스프링 컨테이너를 넘겨줘야한다
+		return new DispatcherServlet();
 	}
-	*/
 }
