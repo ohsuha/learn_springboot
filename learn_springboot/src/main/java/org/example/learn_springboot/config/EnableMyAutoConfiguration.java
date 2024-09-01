74a4396bf04bcba2a981e7cae6fd4561a40aee50
suhaoh <suha.oh91@gmail.com>
2024-09-01 21:16:52 +0900

    불필요한 메타 어노테이션 삭제
    @Configuration
    @ComponentScan MySpringBootApplication 에 있으니 삭제

--- learn_springboot/src/main/java/org/example/learn_springboot/config/EnableMyAutoConfiguration.java
+++ learn_springboot/src/main/java/org/example/learn_springboot/config/EnableMyAutoConfiguration.java
@@ -5,14 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
-@ComponentScan
 @Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
