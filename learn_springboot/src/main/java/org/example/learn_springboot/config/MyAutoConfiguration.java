522221cc1e9153075f3ec381bec65e1c4ec37eeb
suhaoh <suha.oh91@gmail.com>
2024-09-01 21:52:17 +0900

    annotation
    import 해서 자동으로 등록할 인프라 스트럭쳐 빈의 설정 파일 정보를 외부로 빼는 방법

---
+++ learn_springboot/src/main/java/org/example/learn_springboot/config/MyAutoConfiguration.java
@@ -0,0 +1,15 @@
+package org.example.learn_springboot.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Configuration;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration // 어떤 클래스에 붙이면 Configuration 붙인것과 같은 효과
+public @interface MyAutoConfiguration {
+	// 자동 구성방식에 사용할 Configuration 의 목록을 넣을 것
+}
